9a9935fff579f4f1ab203c04a19a30369d458537
Anders Englund <y94anden@hotmail.com>
2019-08-24 13:08:25 +0200

    Clearing more stuff before jumping to bootloader

--- hardware.c
+++ hardware.c
@@ -4,7 +4,10 @@
 #include <avr/interrupt.h>
 
 void initPorts() {
-  DDRB  = (1 << 5); // Pin 5 output = LED
+  DDRB  =
+    (1 << 5) | // Pin 5 output = LED
+    (1 << 6);  // Pin 6 output = WS1812b led chain
+
   PORTB = 0;    // No pullup, output 0
 
   DDRC  = 0x00; // All inputs
