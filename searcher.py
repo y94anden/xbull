93dcac8402fc17d31cdcfb8b046902929915d666
Anders Englund <y94anden@hotmail.com>
2019-09-14 22:40:47 +0200

    Added support to list found units

--- searcher.py
+++ searcher.py
@@ -60,6 +60,8 @@ if __name__ == '__main__':
                         'rounds to run search. Defaults to 5')
     parser.add_argument('-a', '--autoset', action='store_true', help='Autoset '
                         'id of units with id == 0')
+    parser.add_argument('-l', '--list', action='store_true', help='List all '
+                        'responding units when done')
     parser.add_argument('ignored', nargs='*', help='Addresses of units that '
                         'are to be silenced before search')
     args = parser.parse_args()
@@ -79,12 +81,12 @@ if __name__ == '__main__':
         units = s.search()
         print(' ' * 40)
 
+    used = None
     if args.autoset:
-        used = args.ignored
-        used.append(0)  # We do not want this either.
+        used = args.ignored.copy()
         for unit, next_slot in units:
-            if unit in used:
-                suggestions = [i for i in range(255) if i not in used]
+            if unit == 0 or unit in used:
+                suggestions = [i for i in range(1, 255) if i not in used]
                 newid = suggestions[0]
                 print('Addressing 0x%02X unit as 0x%02X' % (unit, newid))
                 # Since multiple units might have the same address, identify
@@ -92,3 +94,31 @@ if __name__ == '__main__':
                 b.write(unit, 0x01, bytes([newid, next_slot]))
                 unit = newid
             used.append(unit)
+
+    if args.list:
+        if used:
+            units = used
+        else:
+            units = [u[0] for u in units]
+
+        print(' ID   Name               Version')
+        for unit in sorted(units):
+            if unit in args.ignored:
+                name = version = '<ignored>'
+            else:
+                try:
+                    data = b.read(unit, 0x02)
+                    name = b''
+                    for d in data:
+                        if d == 0xff or d == 0:
+                            break
+                        name += bytes([d])
+                    name = name.decode()
+                except Exception as e:
+                    print(e)
+                    name = '<unknown>'
+                try:
+                    version = b.read(unit, 0x06).decode()
+                except Exception:
+                    version = '<unknown>'
+            print('0x{:02x}| {: <16} | {}'.format(unit, name, version))
